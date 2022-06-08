dda893e9e494cda5ee9db50b0ed72c3f72ed3355
pedro.viniciusacm@gmail.com <pedro.viniciusacm@gmail.com>
2022-06-08 10:27:57 -0300

    introduction calculadora de imc

---
+++ aprendizado_html_css/calculadora_imc/arquivos_js/calculadora.js
@@ -0,0 +1,16 @@
+// about calculo
+var peso = 89.17;
+var altura = 1.69;
+
+var imc = peso / (altura * altura);
+console.log(imc);
+
+/* passos da lógica
+1 -> capturar nome
+2 -> capturar peso
+3 -> capturar altura
+4 -> calcular imc = p(a*a)
+5 -> devolver a str = "Olá fulano seu imc é xx.xx, você está no xxxxxxx(peso ideal/fora/acima)"
+6 -> apresentar tabela com os cálculos
+7 -> href com o link para fonte.
+*/
