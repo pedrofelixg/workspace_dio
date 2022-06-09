30c16c5b634ca49c8e9ce7eca23482eb363b4c7f
pedro.viniciusacm@gmail.com <pedro.viniciusacm@gmail.com>
2022-06-08 23:07:49 -0300

    update on javascript, html e css

--- aprendizado_html_css/calculadora_imc/arquivos_js/calculadora.js
+++ aprendizado_html_css/calculadora_imc/arquivos_js/calculadora.js
@@ -1,34 +1,30 @@
-// about calculo
+function getValues() {
+    var nome = document.getElementById("name").value;
+    var peso = document.getElementById("peso").value;
+    var altura = document.getElementById("altura").value;
 
-var nome = 'Pedro';
-var peso = 75.50; // get element html
-var altura = 1.69; // get element html
+    var pesoValue = peso;
+    var alturaValue = altura;
 
-var imc = peso / (altura * altura);
+    var imc = parseFloat(pesoValue/(alturaValue * alturaValue));
+    var imcFixo = imc.toFixed(2);
 
-if(imc <= 18.5) {
-    alert('Você esta abaixo do peso ideal');
-    if (imc > 18.5, imc <= 24) {
-        alert('Você está no peso ideal');
-        if (imc > 24, imc <=29.9) {
-            alert('Você está com excesso de peso');
+    var notice = document.getElementById("notice");
+    var calculo = "Olá " + nome + " Seu IMC é " + imcFixo;
+    notice.innerHTML = calculo;
+
+    if(imc <= 18.5) {
+        alert('Você está abaixo do peso ideal');
+        } else if(imc > 18.6, imc <= 24.9) {
+            alert('Você está no peso ideal');
+        } else if(imc >25.0, imc <= 29.9) {
+            alert('Você está com sobrepeso');
+        }  else if(imc > 30.0, imc <= 34.9) {
+            alert('Você está com Obesidade I');
+        } else if(imc > 35.0, imc <= 39.9) {
+            alert('Você está com Obesidade II');
         } else {
-            alert('Você esta com algum grau de obesidade');
-        }
-    }
+            alert('Você está com Obesidade III');
+    }  
 }
 
-console.log(nome);
-console.log(imc);
-
-console.log("Olá", nome, "seu peso ideal é:", imc);
-
-/* passos da lógica
-1 -> capturar nome
-2 -> capturar peso
-3 -> capturar altura
-4 -> calcular imc = p(a*a)
-5 -> devolver a str = "Olá fulano seu imc é xx.xx, você está no xxxxxxx(peso ideal/fora/acima)"
-6 -> apresentar tabela com os cálculos
-7 -> href com o link para fonte.
-*/
